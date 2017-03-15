b869b62a717c98547fda37420301057fa146feeb
James Swineson <i@swineson.me>
2017-03-15 18:40:36 +0800

    remove wilddog

--- assets/main.js
+++ assets/main.js
@@ -3,31 +3,17 @@ function onReady() {
     p = JSPaint();
     p.init(document.getElementById('canvasDiv'));
 
-    // set up sync service
-    var config = { syncURL: "https://jspaint-test-jimu.wilddogio.com" };
-    wilddog.initializeApp(config);
-    var ref = wilddog.sync().ref("/canvas/0");
-    var connectedRef = wilddog.sync().ref("/.info/connected");
-
-    connectedRef.on("value", function (snap) {
-        if (snap.val() === true) {
-            p.updateUserDebugMsg("Online");
-        } else {
-            p.updateUserDebugMsg("Offline");
-        }
-    });
-
     // set up draw event listener
     p.addClickEventListener(function(e, f){
-        ref.child("events").push(e);
-        console.log(ref.child("events"));
+
     });
 
-    // Initial update 
-    // p.doInitialUpdate(ref.child("events"));
-    
-    
-    window.addEventListener('contextmenu', function (e) { e.preventDefault(); e.stopPropagation(); return false; });
+    // disable context menu
+    window.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    });
 }
 
 (function ready(fn) {
