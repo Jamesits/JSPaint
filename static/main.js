76c94c91a4aac4cb5e7cbfc3a1cad4d2a6f58d43
James Swineson <i@swineson.me>
2017-03-16 21:42:22 +0800

    send log when back online

--- static/main.js
+++ static/main.js
@@ -1,12 +1,12 @@
 var onReady = function () {
 
     "use strict";
-    
+
     // global consts
     const roomid = 0;
 
     // set up paint canvas
-    p = JSPaint();
+    var p = JSPaint();
     p.init(document.getElementById('canvasDiv'));
 
     // generate UUID for this client
@@ -18,6 +18,7 @@ var onReady = function () {
 
     // set up websocket
     var ws_status = "disconnected";
+    var ws_is_connected = false;
     var ws_reconnect_interval;
     var ws_timer;
     var ws;
@@ -26,22 +27,29 @@ var onReady = function () {
         p.updateUserDebugMsg(text);
     }
     var ws_location = "ws://" + location.host + (location.pathname.endsWith('/') ? location.pathname.slice(0, -1) : location.pathname) + "/ws";
-    var ws_waiting_list;
+    var ws_waiting_list = [];
     var wsSetup = function () {
         ws = new WebSocket(ws_location + "?room=" + roomid + "&id=" + uuid);
         
 
         // connected
         ws.addEventListener('open', function (event) {
-            ws_status = "connected";
+            ws_status = "connected, sending log...";
+            ws_is_connected = true;
             updateUserDebugMsg();
             console.log("WebSocket connected: ", event);
             ws_reconnect_interval = 0;
+            while (ws_waiting_list.length > 0) {
+                ws.send(ws_waiting_list.shift());
+            }
+            ws_status = "connected";
+            updateUserDebugMsg();
         });
 
         // server disconnect
         ws.addEventListener('close', function (event) {
             ws_status = "disconnected, reconnect interval " + ws_reconnect_interval + "ms";
+            ws_is_connected = false;
             updateUserDebugMsg();
             console.log("WebSocket close: ", event, ws_reconnect_interval);
             ws_timer = setTimeout(wsSetup, ws_reconnect_interval);
@@ -50,6 +58,7 @@ var onReady = function () {
         // connection failure
         ws.addEventListener('error', function (event) {
             ws_status = "error";
+            ws_is_connected = false;
             updateUserDebugMsg();
             console.log("WebSocket error: ", event);
             if (ws_reconnect_interval <= 32000) {
@@ -60,7 +69,12 @@ var onReady = function () {
         // set up draw event listener
         p.clearClickEventListener();
         p.addClickEventListener(function (e, f) {
-            ws.send(JSON.stringify(e));
+            var msg = JSON.stringify(e);
+            if (ws_is_connected) {
+                ws.send(msg);
+            } else {
+                ws_waiting_list.push(msg);
+            }
         });
 
         ws.addEventListener('message', function (event) {
@@ -77,7 +91,7 @@ var onReady = function () {
         e.stopPropagation();
         return false;
     });
-}
+};
 
 (function ready(fn) {
     if (document.readyState != 'loading'){
