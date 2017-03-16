d6dc1b6f1dfaf521f846fb76aabe5f77324e849a
James Swineson <i@swineson.me>
2017-03-17 07:33:30 +0800

    Output some msg when server starts

--- server.py
+++ server.py
@@ -115,3 +115,4 @@ if __name__ == '__main__':
     tornado.ioloop.IOLoop.instance().add_timeout(
         datetime.timedelta(seconds=push_interval), WebSocketHandler.server_push)
     tornado.ioloop.IOLoop.instance().start()
+    print("Server listening on port {}...".format(options.port))
